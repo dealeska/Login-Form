b1738eaddcdafc3ee7f7367ac3dfa0d32ca3e326
Катя Крашник <kotova123451234512345@gmail.com>
2021-06-15 10:19:24 +0300

    changes

--- source/models/Login.ts
+++ source/models/Login.ts
@@ -1,64 +1,66 @@
 import { ObservableObject, reaction, transaction, unobservable } from 'reactronic'
+
 export interface User
 {
-    login: string
-    password: string
+  login: string
+  password: string
 }
+
 export class Authentication extends ObservableObject
 {
-    @unobservable readonly users: User[] = [
-      {login: 'anonimus', password: '12345678'},
-      {login: 'alesya', password: 'aysela'},
-      {login: 'test', password: 'test'},
-      {login: 'login', password: 'pass'}
-    ]
-    state: string
-    login: string
-    password: string
-    constructor(){
-      super()
-      this.login = ''
-      this.password = ''
-      this.state = 'enter something'
-    }
+  @unobservable readonly users: User[] = [
+    {login: 'anonimus', password: '12345678'},
+    {login: 'alesya', password: 'aysela'},
+    {login: 'test', password: 'test'},
+    {login: 'login', password: 'pass'}
+  ]
+  state: string
+  login: string
+  password: string
+  constructor(){
+    super()
+    this.login = ''
+    this.password = ''
+    this.state = 'enter something'
+  }
 
-    @transaction
-    setLogin(userLogin: string): void
+  @transaction
+  setLogin(userLogin: string): void
+  {
+    this.login = userLogin
+  }
+
+  @transaction
+  setPassword(userPassword: string): void
+  {
+    this.password = userPassword
+  }
+
+  @reaction
+  printInfo(): void {
+    if (this.login === '' || this.password === '')
     {
-      this.login = userLogin
+      this.state = 'Enter all data!!!'
     }
-
-    @transaction
-    setPassword(userPassword: string): void
+    else if (this.login !== '')
     {
-      this.password = userPassword
-    }
-
-    @reaction
-    PrintInfo(): void {
-      if (this.login === '' || this.password === '')
-      {
-        this.state = 'Enter all data!!!'
-      }
-      else if (this.login !== '')
-      {
-        this.state = 'Go away, stranger'
-        this.users.forEach(e => {
-          if (e.login === this.login)
+      this.state = 'Go away, stranger'
+      this.users.forEach(e => {
+        if (e.login === this.login)
+        {
+          if (e.password === this.password)
           {
-            if (e.password === this.password)
-            {
-              this.state = 'Your are is a user!'
-            }
-            else
-            {
-              this.state = 'password != your password'
-            }
+            this.state = 'Your are is a user!'
           }
-        })
-      }
-      else{
-        this.state = 'Go away, stranger'
-      }
+          else
+          {
+            this.state = 'password != your password'
+          }
+        }
+      })
     }
+    else{
+      this.state = 'Go away, stranger'
+    }
+  }
 }
